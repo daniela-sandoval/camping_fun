1e2a25d6b3fd1442693496a849b8e149d9bf334b
Daniela Sandoval <daniela.sandoval820@gmail.com>
2019-06-13 23:41:13 -0400

    full crud app

--- app/controllers/campers_controller.rb
+++ app/controllers/campers_controller.rb
@@ -1,5 +1,43 @@
 class CampersController < ApplicationController
+
   def index
     @campers = Camper.all
   end
+
+  def show
+    @camper = Camper.find(params[:id])
+    @active = @camper.activities
+  end
+
+  def new
+    @camper = Camper.new
+  end
+
+  def create
+    @camper = Camper.create(camper_params)
+    redirect_to @camper
+  end
+
+  def edit
+    @camper = Camper.find(params[:id])
+  end
+
+  def update
+    @camper = Camper.find(params[:id])
+    @camper.update(camper_params)
+    redirect_to @camper
+  end
+
+  def destroy
+    @camper = Camper.find(params[:id])
+    @camper.destroy
+    redirect_to campers_path
+  end
+
+  private
+  def camper_params
+    params.require(:camper).permit(:name, :age)
+  end
+
+
 end
